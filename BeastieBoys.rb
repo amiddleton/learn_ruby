dcd373ffd816d82c2e72e1ed9fb90926201ade26
Alan Middleton <alanmiddleton@yahoo.com>
2012-06-18 12:53:41 -0600

    Adding the Class BeastieBoys to learn_ruby

---
+++ BeastieBoys.rb
@@ -0,0 +1,23 @@
+class BeastieBoys
+  def studio_albums
+    studio_albums = ['Licensed to Ill, 1984',
+                     'Paul\'s Boutique, 1989',
+                     'Check Your Head, 1992',
+                     'Ill Communication, 1994',
+                     'Hello Nasty, 1998',
+                     'To the 5 Boroughs, 2004',
+                     'The Mix-up, 2007',
+                     'Hot Sauce Committee Part Two, 2011']
+  end
+  def albums_year
+    albums_year = {
+      '1986' => 'Licensed to Ill',
+      '1989' => 'Paul\'s Boutique',
+      '1992' => 'Check Your Head',
+      '1994' => 'Ill Communication',
+      '1998' => 'Hello Nasty',
+      '2004' => 'To the 5 Boroughs',
+      '2007' => 'The Mix-up',
+      '2011' => 'Hot Sauce Committee Part Two'}
+  end
+end
